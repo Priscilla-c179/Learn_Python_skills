72d79a82044d7ddcae0830bad52ab3d6f90186f8
Priscilla-c179 <muturiw441@gmail.com>
2025-08-20 16:50:00 +0300

    comit changes in the file

---
+++ seaborn_class.py
@@ -0,0 +1,8 @@
+#Load data and import necessary libraries
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+tips=sns.load_dataset('tips')
+tips.head()
+tips
